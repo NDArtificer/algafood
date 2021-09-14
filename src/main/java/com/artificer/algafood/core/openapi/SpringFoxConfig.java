997a46487ac4e62c5d8b5fc83fdbc0e432407865
NDArtificer <The.artificer@hotmail.com>
2021-09-14 20:22:45 -0300

    Ajuste na Documentação do Swagger

--- src/main/java/com/artificer/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/artificer/algafood/core/openapi/SpringFoxConfig.java
@@ -1,14 +1,30 @@
 package com.artificer.algafood.core.openapi;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.artificer.algafood.api.exceptionhandler.Problem;
+import com.fasterxml.classmate.TypeResolver;
+
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RepresentationBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.schema.ModelSpecification;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -20,14 +36,89 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 
 	@Bean
 	public Docket apiDocket() {
+		var typeresolver = new TypeResolver();
+		
 		return new Docket(DocumentationType.OAS_30)
 				.select()
 					.apis(RequestHandlerSelectors.basePackage("com.artificer.algafood.api"))
 					.build()
 					.apiInfo(apiInfo())
+					.useDefaultResponseMessages(false)
+					.globalResponses(HttpMethod.GET, globalGetResponseMessages())
+					.globalResponses(HttpMethod.POST, globalPostPutResponseMessages())
+		            .globalResponses(HttpMethod.PUT, globalPostPutResponseMessages())
+		            .globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
+		            .additionalModels(typeresolver.resolve(Problem.class))
 					.tags(new Tag("Cidades", "Gerencia as Cidades"));
 	}
 	
+	private List<Response> globalGetResponseMessages() {
+		return Arrays.asList(
+				new ResponseBuilder()
+				.code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+				.description("Erro interno do Servidor")
+				.build(),
+				
+				new ResponseBuilder()
+				.code(String.valueOf( HttpStatus.NOT_ACCEPTABLE.value()))
+				.description("Tipo de Reposta não possui representação que possa ser aceita")
+				.build()
+				);
+	}
+	
+	
+	private List<Response> globalPostPutResponseMessages() {
+		return Arrays.asList(
+				new ResponseBuilder()
+				.code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+				.description("Erro interno do Servidor")
+				.build(),
+				
+				new ResponseBuilder()
+				.code(String.valueOf( HttpStatus.NOT_ACCEPTABLE.value()))
+				.description("Tipo de Reposta não possui representação que possa ser aceita")
+				.build(),
+				
+				new ResponseBuilder()
+				.code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+				.description("Requisição inválida (erro do cliente)")
+				.representation( MediaType.APPLICATION_JSON)
+					.apply(builderModelProblema())
+				.build(),
+				
+				new ResponseBuilder()
+				.code(String.valueOf( HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()))
+				.description("Requisição recusada porque o corpo está em um formato não suportado")
+				.build()
+				);
+	}
+	
+	
+	private Consumer<RepresentationBuilder> builderModelProblema() {
+		return r->r.model(m->m.name("Problem")
+				.referenceModel(
+						ref->ref.key(
+								k->k.qualifiedModelName(
+										q->q.name("Problem")
+										.namespace("com.artificer.algafood.api.exceptionhandler")
+										))));
+	}
+
+	private List<Response> globalDeleteResponseMessages() {
+		return Arrays.asList(
+				new ResponseBuilder()
+				.code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+				.description("Erro interno do Servidor")
+				.build(),
+				
+				new ResponseBuilder()
+				.code(String.valueOf( HttpStatus.BAD_REQUEST.value()))
+				.description("Requisição inválida (erro do cliente)")
+				.build()
+				);
+	}
+	
+
 	public ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
 				.title("AlgaFood API")
