41bc1a68e8626694bbe02b5b741ae2b2cc120e99
NDArtificer <The.artificer@hotmail.com>
2022-01-28 20:06:24 -0300

    Ajuste pra dockerizar a aplicação

--- src/main/java/com/artificer/algafood/core/io/Base64ProtocolResolver.java
+++ src/main/java/com/artificer/algafood/core/io/Base64ProtocolResolver.java
@@ -2,23 +2,16 @@ package com.artificer.algafood.core.io;
 
 import java.util.Base64;
 
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.boot.context.event.ApplicationContextInitializedEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.ProtocolResolver;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.stereotype.Component;
 
-@Component
 public class Base64ProtocolResolver
-		implements ProtocolResolver, ApplicationContextInitializer<ConfigurableApplicationContext> {
+		implements ProtocolResolver, ApplicationListener<ApplicationContextInitializedEvent> {
 
-	@Override
-	public void initialize(ConfigurableApplicationContext applicationContext) {
-		((ConfigurableApplicationContext) applicationContext).addProtocolResolver(this);
-
-	}
 
 	@Override
 	public Resource resolve(String location, ResourceLoader resourceLoader) {
@@ -27,6 +20,12 @@ public class Base64ProtocolResolver
 			return new ByteArrayResource(decodedResource);
 		}
 		return null;
+	}
+
+	@Override
+	public void onApplicationEvent(ApplicationContextInitializedEvent event) {
+		event.getApplicationContext().addProtocolResolver(this);
+		
 	}
 
 }
