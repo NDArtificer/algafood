2b51ecda69525de0494d51dec5a72f0ebe6ff1e0
Denilson <Denilson@DESKTOP-PODQIC4>
2022-12-28 16:21:26 -0300

    Implemtação SpringDoc

--- src/main/java/com/artificer/algafood/api/openapi/controller/CidadeControllerOpenApi.java
+++ src/main/java/com/artificer/algafood/api/openapi/controller/CidadeControllerOpenApi.java
@@ -1,12 +1,14 @@
 package com.artificer.algafood.api.openapi.controller;
 
-
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 
 import com.artificer.algafood.api.model.CidadeModel;
 import com.artificer.algafood.api.model.input.CidadeInput;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -14,13 +16,21 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "Cidades")
 public interface CidadeControllerOpenApi {
 
+	@Operation(summary = "Listar as cidades")
 	CollectionModel<CidadeModel> listar();
 
-	CidadeModel buscar(Long cidadeId);
+	@Operation(summary = "Buscar uma cidade por Id")
+	CidadeModel buscar(@Parameter(description = "Id de uma Cidade", example = "1", required = true) Long cidadeId);
 
-	CidadeModel adicionar(CidadeInput cidadeInput);
+	@Operation(summary = "Adiciona uma cidade por Id", description = "Cadastro de cidade necessita de um estado váliddo e um nome de cidade válido")
+	CidadeModel adicionar(
+			@RequestBody(description = "Repesentação de uma nova Cidade", required = true) CidadeInput cidadeInput);
 
-	CidadeModel atualizar(Long cidadeId, CidadeInput cidadeInput);
+	@Operation(summary = "Atualiza uma cidade por Id", description = "Atulização de cidade necessita de um estado váliddo e um Id e nome de cidade válido")
+	CidadeModel atualizar(@Parameter(description = "Id de uma Cidade", example = "1", required = true) Long cidadeId,
+			@RequestBody(description = "Repesentação de uma Cidade", required = true) CidadeInput cidadeInput);
 
-	ResponseEntity<Void> remover(Long cidadeId);
+	@Operation(summary = "Remove uma cidade por Id")
+	ResponseEntity<Void> remover(
+			@Parameter(description = "Id de uma Cidade", example = "1", required = true) Long cidadeId);
 }
--- src/main/java/com/artificer/algafood/core/springdoc/SpringDocConfig.java
+++ src/main/java/com/artificer/algafood/core/springdoc/SpringDocConfig.java
@@ -1,7 +1,9 @@
 package com.artificer.algafood.core.springdoc;
 
+import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.OAuthFlow;
@@ -11,28 +13,39 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
 
 @Configuration
-@SecurityScheme(name = "security_auth",
-				type = SecuritySchemeType.OAUTH2,
-				flows = @OAuthFlows(authorizationCode = @OAuthFlow(
-						authorizationUrl = "${springdoc.oauthflow.authorizationurl}",
-						tokenUrl = "${springdoc.oauthflow.tokenurl}",
-						scopes = {
-								@OAuthScope(name = "READ", description = "read scope"),
-								@OAuthScope(name = "WRITE", description = "write scope")
-						})))
-public class SpringDocConfig  {
+@SecurityScheme(name = "security_auth", type = SecuritySchemeType.OAUTH2, flows = @OAuthFlows(authorizationCode = @OAuthFlow(authorizationUrl = "${springdoc.oauthflow.authorizationurl}", tokenUrl = "${springdoc.oauthflow.tokenurl}", scopes = {
+		@OAuthScope(name = "READ", description = "read scope"),
+		@OAuthScope(name = "WRITE", description = "write scope") })))
+public class SpringDocConfig {
 
 	@Bean
 	public OpenAPI openApi() {
-		return new OpenAPI()
-				.info(new Info().title("AlgaFood API")
-								.version("v1")
-								.description("Rest API do AlgaFood")
-								.license(new License()
-										.name("Apache 2.0")
-										.url("http://springdoc.com ")));
+		return new OpenAPI().info(new Info().title("AlgaFood API").version("v1").description("Rest API do AlgaFood")
+				.license(new License().name("Apache 2.0").url("http://springdoc.com ")));
+	}
+
+	@Bean
+	public OpenApiCustomiser openApiCustomiser() {
+		return openApi -> {
+			openApi.getPaths().values().stream().flatMap(pathItem -> pathItem.readOperations().stream())
+					.forEach(operation -> {
+						var responses = operation.getResponses();
+						var apiResponsesNaoEncontrado = new ApiResponse().description("Recurso Não Encontrado");
+						var apiResponsesErrorInterno = new ApiResponse().description("Erro Interno Servidor");
+						var apiResponsesSemRepresentacao = new ApiResponse()
+								.description("Recurso sem representação que poderia ser aceita pelo consumidor");
+
+						responses
+								.addApiResponse(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()),
+										apiResponsesErrorInterno)
+								.addApiResponse(String.valueOf(HttpStatus.NOT_FOUND.value()), apiResponsesNaoEncontrado)
+								.addApiResponse(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()),
+										apiResponsesSemRepresentacao);
+					});
+		};
 	}
 
 }
